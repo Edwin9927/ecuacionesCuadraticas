76e07136d9e2d851e95a29e0b92be45edef2883a
Edwin9927 <59030718+Edwin9927@users.noreply.github.com>
2022-05-05 22:02:03 -0500

    Version 2.0

--- main.py
+++ main.py
@@ -1,91 +1,113 @@
 import numpy as np
 from matplotlib import pyplot
 
-# Variables de control de datos
-swa = 0
-swb = 0
-swc = 0
 
-# Control de la variable a
-while swa == 0:
+def leerNumero(id):
     try:
-        a = float(input("Ingrese el valor de (a):\n"))
-        if a == 0:
-            raise Exception("a no puede ser zero")
-    except:
-        print("!!! Numero no valido !!!\n Vuelva a ingresarlo")
+        num = float(input("Ingrese el valor de " + id + "\n"))
+        if id == "a" and num == 0:
+            raise Exception("a no puede ser cero")
+        return num
+    except Exception as e:
+        print(str(e))
+        return None
+
+
+def calcularDiscriminate(a, b, c):
+    return (b ** 2) - 4 * a * c
+
+
+def calcularRaizPositiva(a, b, det):
+    return ((-b) + (det ** 0.5)) / (2 * a)
+
+
+def calcularRaizNegativa(a, b, det):
+    return ((-b) - (det ** 0.5)) / (2 * a)
+
+
+def formatoEcuacion(a, b, c):
+    if a == 1:
+        title = "x²"
     else:
-        swa = 1
+        title = str(a) + "x²"
+    if b != 0:
+        title = title + str("{:+}".format(b)) + "x"
+    if c != 0:
+        title = title + str("{:+}".format(c))
+    return title
 
-# Control de la variable b
-while (swb == 0):
-    try:
-        b = float(input("Ingrese el valor de (b):\n"))
-    except:
-        print("!!! Numero no valido !!!\n Vuelva a ingresarlo")
+
+def graficar(a, b, c):
+    x = np.arange(-20, 20, 0.1)
+    y = (a * (x ** 2)) + (b * x) + c
+
+    pyplot.axhline(0, color="black")
+    pyplot.axvline(0, color="black")
+
+    # Calculo del vertice de la parabola
+    xv = ((-b) / (2 * a))
+    yv = (a * (xv ** 2)) + (b * xv) + c
+    # Forma canonica f(x) = a(x-xv)²+yv
+
+    if a > 0:
+        pyplot.ylim(yv - 2, yv + 15)
     else:
-        swb = 1
+        pyplot.ylim(yv - 15, yv + 2)
 
-# Control de la variable c
-while (swc == 0):
-    try:
-        c = float(input("Ingrese el valor de (c):\n"))
-    except:
-        print("!!! Numero no valido !!!\n Vuelva a ingresarlo")
+    if 0 < a < 1:
+        pyplot.xlim(xv - 15, xv + 15)
     else:
-        swc = 1
+        pyplot.xlim(xv - 5, xv + 5)
 
-print("La ecuación de la forma: ", a, "x² {0:+} x".format(b), "{0:+}".format(c), " :")
+    pyplot.plot(x, y)
+    pyplot.plot(xv, yv, marker="o", color="black")
 
-# Calculo del Discriminante
-disc = (b ** 2) - (4 * a * c)
+    pyplot.xlabel('x')
+    pyplot.ylabel('y')
 
-# Verificacion del valor del discriminante
-if disc >= 0:
-    raiz = (disc) ** (0.5)
-else:
-    raiz = (disc * -1) ** (0.5)
+    pyplot.title(formatoEcuacion(a, b, c))
 
-# Calculo de las raices de la ecuacion
-if (disc > 0):
-    print("Raices reales: ")
-    x1 = ((-b) + raiz) / (2 * a)
-    x2 = ((-b) - raiz) / (2 * a)
-    print("x1= ", "{:.3f}".format(x1))
-    print("x2= ", "{:.3f}".format(x2))
-elif (disc == 0):
-    print("Raiz unica")
-    x1 = ((-b) + raiz) / (2 * a)
-    x2 = ((-b) - raiz) / (2 * a)
-    print("x1= ", "{:.3f}".format(x1))
-else:
-    print("Raices imaginarias:")
-    x1 = (-b) / (2 * a)
-    x2 = (-b) / (2 * a)
-    print("x1= ", "{:.3f}".format(x1), "+ ", "{:.3f}".format(raiz), "i")
-    print("x2= ", "{:.3f}".format(x2), "- ", "{:.3f}".format(raiz), "i")
-
-x = np.arange(-20, 20, 0.1)
-y = (a * (x ** 2)) + (b * x) + c
-
-pyplot.axhline(0, color="black")
-pyplot.axvline(0, color="black")
-
-# Calculo del vertice de la parabola
-xv = ((-b) / (2 * a))
-yv = (a * (xv ** 2)) + (b * xv) + c
-# Forma canonica f(x) = a(x-xv)²+yv
+    pyplot.savefig("output.png")
+    pyplot.show()
 
 
-if a > 0:
-    pyplot.ylim(yv - 2, yv + 15)
-else:
-    pyplot.ylim(yv - 15, yv + 2)
+def main():
+    while True:
+        a = leerNumero("a")
+        if a is not None:
+            break
 
-pyplot.xlim(xv - 5, xv + 5)
+    while True:
+        b = leerNumero("b")
+        if b is not None:
+            break
 
-pyplot.plot(x, y)
-pyplot.plot(xv, yv, marker="o", color="black")
+    while True:
+        c = leerNumero("c")
+        if c is not None:
+            break
 
-pyplot.savefig("output.png")
-pyplot.show()
+    disc = calcularDiscriminate(a, b, c)
+
+    x1 = calcularRaizPositiva(a, b, disc)
+    x2 = calcularRaizNegativa(a, b, disc)
+
+    print("La ecuación de la forma: ", formatoEcuacion(a, b, c))
+
+    if disc > 0:
+        print("Raices reales: ")
+        print("x1= ", "{:.3f}".format(x1))
+        print("x2= ", "{:.3f}".format(x2))
+    elif disc == 0:
+        print("Raiz unica")
+        print("x1= ", "{:.3f}".format(x1))
+    else:
+        print("Raices imaginarias:")
+        print("x1=", "{:.3f}".format(x1.real), "{0:+.3f}".format(x1.imag), "i")
+        print("x2=", "{:.3f}".format(x2))
+
+    graficar(a, b, c)
+
+
+if __name__ == "__main__":
+    main()
